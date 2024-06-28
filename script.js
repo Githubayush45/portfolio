5b7cf30f4db9b7cbd5236fdb8dc09d1feaf17a85
ayuush <ayuush@gmail.com>
2024-06-29 00:41:38 +0530

    ok

---
+++ script.js
@@ -0,0 +1,16 @@
+
+        var modal = document.getElementById("contactModal");
+        var btn = document.querySelector(".btn.btn-primary");
+        var span = document.getElementsByClassName("close")[0];
+        btn.onclick = function() {
+          modal.style.display = "block";
+        }
+        span.onclick = function() {
+          modal.style.display = "none";
+        }
+        window.onclick = function(event) {
+          if (event.target == modal) {
+            modal.style.display = "none";
+          }
+        }
+     
