5e9f909fb1deb3c064abb037c733d38607bd919a
ayuush <ayuush@gmail.com>
2024-07-04 01:15:10 +0530

    po

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-
-        var modal = document.getElementById("contactModal");
-        var btn = document.querySelector(".btn.btn-primary");
-        var span = document.getElementsByClassName("close")[0];
-        btn.onclick = function() {
-          modal.style.display = "block";
-        }
-        span.onclick = function() {
-          modal.style.display = "none";
-        }
-        window.onclick = function(event) {
-          if (event.target == modal) {
+document.addEventListener('DOMContentLoaded', (event) => {
+    var modal = document.getElementById("contactModal");
+    var btn = document.querySelector(".btn.btn-primary");
+    var span = document.getElementsByClassName("close")[0];
+    btn.onclick = function(event) {
+        event.preventDefault(); 
+        event.stopPropagation(); 
+        modal.style.display = "block";
+    }
+    span.onclick = function(event) {
+        event.preventDefault(); 
+        event.stopPropagation(); 
+        modal.style.display = "none";
+    }
+    window.onclick = function(event) {
+        if (event.target == modal) {
             modal.style.display = "none";
-          }
         }
-     
+    }});
+    
